6e1200c1e31ac502e2af781dbb7094c1c3fafcee
Aidan Lister <aidanlister@gmail.com>
2009-02-17 03:27:59 +1100

    Complete rewrite moving the bulk of the work to helper functions
    The new design allows you to implement any of the paypal calls
    in a few lines. The rest of the work should be done in the view.

--- PayPal.py
+++ PayPal.py
@@ -1,28 +1,39 @@
 import urllib, md5, datetime
 from cgi import parse_qs
 from django.core.urlresolvers import reverse
-from django.conf import settings
+from django_paypalcart import default_settings as settings
 
-class CartError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return str(self.msg)
-
-class PayPal:    
-    URL_BASE      = 'http://127.0.0.1:8000'
-    URL_CHECKOUT  = 'https://www.sandbox.paypal.com/webscr&cmd=_express-checkout&token='
-    URL_NVP       = 'https://api-3t.sandbox.paypal.com/nvp'
+class PayPal: 
+    base_nvps = {}
+    express_checkout_url = ''
+    interface_url = ''
 
     def __init__(self):
-        signature_values = {
-            'USER':      settings.PAYPALCART_API_USER,
-            'PWD':       settings.PAYPALCART_API_PASSWORD,
-            'SIGNATURE': settings.PAYPALCART_API_SIGNATURE,
-            'VERSION':   '53.0',
+        self.base_nvps = {
+            'USER':       settings.PAYPALCART_API_USER,
+            'PWD':        settings.PAYPALCART_API_PASSWORD,
+            'SIGNATURE':  settings.PAYPALCART_API_SIGNATURE,
+            'VERSION':    '53.0',
             }
-        self.signature = urllib.urlencode(signature_values) + "&"
+        
+        map = {'live':'', 'sandbox':'.sandbox', 'sandbox-beta':'.sandbox-beta'}
+        self.express_checkout_url = \
+            'https://www%s.paypal.com/webscr&cmd=_express-checkout&token=' % \
+            map[settings.PAYPALCART_ENVIRONMENT]
+            
+        self.interface_url = 'https://api-3t%s.paypal.com/nvp' % \
+            map[settings.PAYPALCART_ENVIRONMENT]
+        
+    def __request(self, nvps):
+        """Perform the NVP request and return the response"""
+        params = self.base_nvps
+        params.update(nvps)
+        nvps = dict((k.upper(), v) for k, v in params.items())
+        response = urllib.urlopen(self.interface_url, urllib.urlencode(nvps)).read()
+        response = parse_qs(response)
+        response = dict((k, v[0]) for k, v in response.items())
+        return response
+            
             
     ### Authorization and Capture API
     def DoCapture(self):
@@ -43,148 +54,60 @@ class PayPal:
     
     
     ### DoDirectPayment API
-    def DoDirectPayment(self, amt, ipaddress, acct, expdate, cvv2, firstname, lastname, cctype, street, city, state, zipcode):
+    def DoDirectPayment(self):
         """Process a credit card payment"""
-        params = {
-            'METHOD' : "DoDirectPayment",
-            'PAYMENTACTION' : 'Sale',
-            'AMT' : amt,
-            'IPADDRESS' : ipaddress,
-            'ACCT': acct,
-            'EXPDATE' : expdate,
-            'CVV2' : cvv2,
-            'FIRSTNAME' : firstname,
-            'LASTNAME': lastname,
-            'CREDITCARDTYPE': cctype,
-            'STREET': street,
-            'CITY': city,
-            'STATE': state,
-            'ZIP':zipcode,
-            'COUNTRY' : 'United States',
-            'COUNTRYCODE': 'US',
-            'RETURNURL' : 'http://www.yoursite.com/returnurl', #edit this 
-            'CANCELURL' : 'http://www.yoursite.com/cancelurl', #edit this 
-            'L_DESC0' : "Desc: ",
-            'L_NAME0' : "Name: ",
-        }
-        params_string = self.signature + urllib.urlencode(params)
-        response = urllib.urlopen(self.API_ENDPOINT, params_string).read()
-        response_tokens = {}
-        for token in response.split('&'):
-            response_tokens[token.split("=")[0]] = token.split("=")[1]
-        for key in response_tokens.keys():
-            response_tokens[key] = urllib.unquote(response_tokens[key])
-        return response_tokens
+        pass
 
     
     ### Express Checkout API
-    def SetExpressCheckout(self, amount, return_url, cancel_url, **kwargs):
+    def SetExpressCheckout(self, returnurl, cancelurl, amt, **kwargs):
         """Initiates an Express Checkout transaction
         
         Optionally, the SetExpressCheckout API operation can set up billing agreements for 
         reference transactions and recurring payments.
         """
-        return_url = self.URL_BASE + reverse(return_url)
-        cancel_url = self.URL_BASE + reverse(cancel_url)
-        params = {
-            'METHOD':        "SetExpressCheckout",
-            'NOSHIPPING':    1,
-            'PAYMENTACTION': 'Sale',
-            'RETURNURL':     return_url,
-            'CANCELURL':     cancel_url,
-            'AMT':           amount,
-        }
-        params.update(kwargs)
-        params_string = self.signature + urllib.urlencode(params)
-        try:
-            response = urllib.urlopen(self.URL_NVP, params_string).read()
-            response_dict = parse_qs(response)
-            assert response_dict['ACK'][0] == 'Success'
-            response_token = response_dict['TOKEN'][0]
-        except (AssertionError, IOError, KeyError):
-            raise CartError(response_dict)
-        return response_token
+        nvps = locals()
+        nvps['method'] = 'SetExpressCheckout' 
+        nvps.pop('self')
+        return self.__request(nvps)
     
     
     def GetExpressCheckoutDetails(self, token, **kwargs):
         """Obtain information about an Express Checkout transaction"""
-        params = {
-            'METHOD':    "GetExpressCheckoutDetails",
-            'TOKEN' :    token,
-        }
-        params.update(kwargs)
-        params_string = self.signature + urllib.urlencode(params)
-        try:
-            response = urllib.urlopen(self.URL_NVP, params_string).read()
-            response_dict = parse_qs(response)
-        except (IOError, KeyError):
-            raise PayPalException({})
-        return response_dict
+        nvps = locals()
+        nvps['method'] = 'GetExpressCheckoutDetails' 
+        nvps.pop('self')
+        return self.__request(nvps)
     
     
-    def DoExpressCheckoutPayment(self, token, payerid, amt):
+    def DoExpressCheckoutPayment(self, token, paymentaction, payerid, amt, **kwargs):
         """Completes an Express Checkout transaction
         
         If you set up a billing agreement in your SetExpressCheckout API call, the billing 
         agreement is created when you call the DoExpressCheckoutPayment API operation.
         """
-        params = {
-            'METHOD' : "DoExpressCheckoutPayment",
-            'PAYMENTACTION' : 'Sale',
-            'TOKEN' : token,
-            'AMT' : amt,
-            'PAYERID': payerid,
-        }
-        params_string = self.signature + urllib.urlencode(params)
-        response = urllib.urlopen(self.URL_NVP, params_string).read()
-        response_tokens = {}
-        for token in response.split('&'):
-            response_tokens[token.split("=")[0]] = token.split("=")[1]
-        for key in response_tokens.keys():
-            response_tokens[key] = urllib.unquote(response_tokens[key])
-        return response_tokens
+        nvps = locals()
+        nvps['method'] = 'DoExpressCheckoutPayment' 
+        nvps.pop('self')
+        return self.__request(nvps)
       
           
     ### GetTransactionDetails API
-    def GetTransactionDetails(self, tx_id):
+    def GetTransactionDetails(self, transactionid, **kwargs):
         """Obtain information about a specific transaction"""
-        params = {
-            'METHOD' : "GetTransactionDetails", 
-            'TRANSACTIONID' : tx_id,
-        }
-        params_string = self.signature + urllib.urlencode(params)
-        response = urllib.urlopen(self.API_ENDPOINT, params_string).read()
-        response_tokens = {}
-        for token in response.split('&'):
-            response_tokens[token.split("=")[0]] = token.split("=")[1]
-        for key in response_tokens.keys():
-                response_tokens[key] = urllib.unquote(response_tokens[key])
-        return response_tokens
+        nvps = locals()
+        nvps['method'] = 'GetTransactionDetails' 
+        nvps.pop('self')
+        return self.__request(nvps)
                 
     
     ### MassPay API
-    def MassPay(self, email, amt, note, email_subject):
+    def MassPay(self, **kwargs):
         """Make a payment to one or more PayPal account holders"""
-        unique_id = str(md5.new(str(datetime.datetime.now())).hexdigest())
-        params = {
-            'METHOD' : "MassPay",
-            'RECEIVERTYPE' : "EmailAddress",
-            'L_AMT0' : amt,
-            'CURRENCYCODE' : 'USD',
-            'L_EMAIL0' : email,
-            'L_UNIQUE0' : unique_id,
-            'L_NOTE0' : note,
-            'EMAILSUBJECT': email_subject,
-        }
-        params_string = self.signature + urllib.urlencode(params)
-        response = urllib.urlopen(self.API_ENDPOINT, params_string).read()
-        response_tokens = {}
-        for token in response.split('&'):
-            response_tokens[token.split("=")[0]] = token.split("=")[1]
-        for key in response_tokens.keys():
-                response_tokens[key] = urllib.unquote(response_tokens[key])
-        response_tokens['unique_id'] = unique_id
-        return response_tokens
+        nvps = locals()
+        nvps['method'] = 'MassPay' 
+        nvps.pop('self')
+        return self.__request(nvps)
               
               
     ### RefundTransaction API
@@ -206,20 +129,7 @@ class PayPal:
         You must invoke the CreateRecurringPaymentsProfile API operation for each profile 
         you want to create. The API operation creates a profile and an associated billing agreement.
         """
-        params = {
-            'METHOD': 'CreateRecurringPaymentsProfile',
-            'PROFILESTARTDATE': startdate,
-            'DESC':desc,
-            'BILLINGPERIOD':period,
-            'BILLINGFREQUENCY':freq,
-            'AMT':amt,
-            'TOKEN':token,
-            'CURRENCYCODE':'USD',
-        }
-        params_string = self.signature + urllib.urlencode(params)
-        response = urllib.urlopen(self.API_ENDPOINT, params_string).read()
-        response_dict = parse_qs(response)
-        return response_dict
+        pass
         
     def GetRecurringPaymentsProfileDetails(self):
         """Obtain information about a recurring payments profile"""
@@ -242,7 +152,7 @@ class PayPal:
         pass
         
     def GetBillingAgreementCustomerDetails(self):
-        """Obtain information about a billing agreement’s PayPal account holder"""
+        """Obtain information about a billing agreement?s PayPal account holder"""
         pass
         
     def BAUpdate(self):
@@ -250,7 +160,7 @@ class PayPal:
         pass
         
     def DoReferenceTransaction(self):
-        """Process a payment from a buyer’s account, which is identified by a previous transaction"""
+        """Process a payment from a buyer?s account, which is identified by a previous transaction"""
         pass
         
         
