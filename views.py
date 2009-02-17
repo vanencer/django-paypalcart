3c66e4961bbca971ccbfd6a53aca817fd3f15d50
Aidan Lister <aidanlister@gmail.com>
2009-02-17 19:01:15 +1100

    Add common usage error handling to views

--- views.py
+++ views.py
@@ -7,30 +7,51 @@ def checkout(request):
     if request.method == "POST":  
         baseurl = request.build_absolute_uri().replace('http://', '')
         baseurl = 'http://' + baseurl.split('/', 2)[0]
-        
         paypal = PayPal.PayPal()
-        response = paypal.SetExpressCheckout(
-            cancelurl=baseurl + reverse('django_paypalcart.views.cancel'),
-            returnurl=baseurl + reverse('django_paypalcart.views.confirm'),
-            amt=request.POST['amount'],
-            currencycode=request.POST['amount'])
+        try:
+            response = paypal.SetExpressCheckout(
+                cancelurl    = baseurl + reverse('django_paypalcart.views.cancel'),
+                returnurl    = baseurl + reverse('django_paypalcart.views.confirm'),
+                amt          = request.POST['amt'],
+                currencycode = request.POST['currencycode'])
+            assert response['ACK'] == 'Success'
+            assert response['TOKEN']
+        except IOError:
+            return render_to_response('error.html', {'type':'IOError'})
+        except KeyError, e:
+            return render_to_response('error.html', {'type':'KeyError'})
+        except AssertionError, e:
+            return render_to_response('error.html', {'type':'ProcessingError', 'response':response})
         
-        if response['ACK'] == 'Success':
-            url = paypal.express_checkout_url + response['TOKEN']
-            #url += '&useraction=commit' # Use to skip the confirm page
-            return HttpResponseRedirect(url)
-    
+        url = paypal.express_checkout_url + response['TOKEN']
+        #url += '&useraction=commit' # Use to skip the confirm page
+        return HttpResponseRedirect(url)
+            
     return render_to_response('checkout.html')
     
     
 def success(request):
     paypal = PayPal.PayPal()
-    result = paypal.DoExpressCheckoutPayment(
-        paymentaction='Sale',
-        token=request.POST['token'],
-        payerid=request.POST['payerid'],
-        amt=request.POST['amount'])
-    params = {'data':result}
+    
+    try:
+        response = paypal.DoExpressCheckoutPayment(
+            paymentaction='Sale',
+            token=request.POST['token'],
+            payerid=request.POST['payerid'],
+            amt=request.POST['amount'])
+        assert response['ACK'] == 'Success'
+    except IOError:
+        return render_to_response('error.html', {'type':'IOError'})
+    except KeyError, e:
+        return render_to_response('error.html', {'type':'KeyError'})
+    except AssertionError, e:
+        return render_to_response('error.html', {'type':'ProcessingError', 'response':response})
+
+    # Everything seems to have gone okay and the customer is charged
+    # We might want to check PAYMENTSTATUS == Completed | Pending and decide what to do
+    
+    
+    params = {'response':response}
     return render_to_response('success.html', params)
 
     
@@ -40,6 +61,15 @@ def cancel(request):
 
 def confirm(request):
     paypal = PayPal.PayPal()
-    data = paypal.GetExpressCheckoutDetails(token=request.GET['token'])
-    params = {'data':data, 'success_url':reverse('django_paypalcart.views.success')}
+    try:
+        response = paypal.GetExpressCheckoutDetails(token=request.GET['token'])
+        assert response['ACK'] == 'Success'
+    except IOError:
+        return render_to_response('error.html', {'type':'IOError'})
+    except KeyError, e:
+        return render_to_response('error.html', {'type':'KeyError'})
+    except AssertionError, e:
+        return render_to_response('error.html', {'type':'ProcessingError', 'response':response})
+
+    params = {'response':response, 'success_url':reverse('django_paypalcart.views.success')}
     return render_to_response('confirm.html', params)
